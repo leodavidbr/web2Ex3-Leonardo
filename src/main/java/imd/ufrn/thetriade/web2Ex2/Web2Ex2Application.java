7a04bd8a8b499297e132fe4b8bf085fb07881f50
leodavidbr <rbdivadoel@gmail.com>
2023-09-01 18:25:04 -0300

    initial commit

---
+++ src/main/java/imd/ufrn/thetriade/web2Ex2/Web2Ex2Application.java
@@ -0,0 +1,13 @@
+package imd.ufrn.thetriade.web2Ex2;
+
+import org.springframework.boot.SpringApplication;
+	import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2Ex2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web2Ex2Application.class, args);
+	}
+
+}
